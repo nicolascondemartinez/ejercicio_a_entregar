e40f52e73a3a2ae01d51b7479c60b2fac93084d3
nicolascondemartinez <nicolasconde204@gmail.com>
2025-04-03 23:50:55 -0300

    Cambio la función que iba a hacer originalmente por el primer boceto de una función que procese cada ronda para ya obtener MVP's

--- src/funciones.py
+++ src/funciones.py
@@ -1,25 +1,24 @@
 
-def puntaje (kills, assist, dead):
+# Calcula puntaje bajo la lógica: 3 puntos por kill, 1 por asistencia y 1 restado por muerte
+def calcular_puntaje (kills, assist, dead):
         score= kills * 3 + assist - (1 if dead else 0)
         return score
 
-def tabla_de_puntajes(rounds):
-    for i, ronda in enumerate(rounds):
-        print(f"\n Ronda {i+1}")
-        print(f"{'Jugador':<10} {'Kills':<6} {'Asistencias':<10} {'Muertes':<8} {'Puntos':<6}")
-        print("-" * 45)
+# Proceso una ronda para devolverla con los MVP's y ordenada de forma descendente
+def procesar_ronda(ronda):
+    # Genero una lista vacía que voy a cargar con tuplas con los datos de cada jugador
+    datos = []
+    
+    # Recorro los diccionarios de cada ronda cargando el código y obteniendo el valor
+    for jugador, stats in ronda.items():
+        kills = stats['kills']
+        assists = stats['assists']
+        deaths = stats['deaths']
+        puntos = calcular_puntaje(kills, assists, deaths) # Llamo a mi función que calcula los puntos
+        datos.append((jugador, kills, assists, deaths, puntos)) # Cargo a la lista tuplas con los datos de cada jugador
+    
+    max_puntos = max(p[4] for p in datos) # Compara los puntos de cada jugador y devuelve el máximo
+    mvps_ronda = [jugador for jugador, _, _, _, puntos in datos if puntos == max_puntos] # Guardo al que salió MVP en cada ronda para usarlo en la impresión final
+    datos_ordenados = sorted(datos, key=lambda x: x[4], reverse=True) # Ordeno los datos de forma descendente
+    return datos_ordenados, mvps_ronda
 
-        # Crear lista de tuplas con estadísticas por ronda
-        estadisticas = [
-            (jugador, stats['kills'], stats['assists'], "Sí" if stats['deaths'] else "No", puntaje(stats['kills'], stats['assists'], stats['deaths']))
-            for jugador, stats in ronda.items()
-        ]
-        
-        
-        # Ordenar por puntos en orden descendente
-        estadisticas.sort(key=lambda x: x[4], reverse=True)
-
-        # Imprimir estadísticas de la ronda
-        for jugador, kills, assists, deaths, puntos in estadisticas:
-            print(f"{jugador:<10} {kills:<6} {assists:<10} {deaths:<8} {puntos:<6}")
-        
